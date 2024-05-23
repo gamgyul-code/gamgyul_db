93c13549a894c9683b12e8a6676b95ff0ff4ae58
gywns0417 <gywns0417@gmail.com>
2024-05-23 11:53:32 +0000

    pleeeease

--- k8s/configs/init.sql
+++ k8s/configs/init.sql
@@ -1,10 +1,10 @@
-CREATE SCHEMA IF NOT EXISTS `krampoline` DEFAULT CHARACTER SET utf8mb4;
+CREATE SCHEMA IF NOT EXISTS `halmang` DEFAULT CHARACTER SET utf8mb4;
 
 GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION;
-GRANT ALL ON krampoline.* TO 'root'@'localhost';
+GRANT ALL ON halmang.* TO 'root'@'localhost';
 FLUSH PRIVILEGES;
 
-USE `krampoline`;
+USE `halmang`;
 
 DROP TABLE IF EXISTS `sample_data`;
 CREATE TABLE `sample_data` (
